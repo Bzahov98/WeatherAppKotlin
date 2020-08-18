b159ac68336ec705ae4422cc94fd47f87b47a63c
bozhidar.zahov <bzahov1998@gmail.com>
2020-08-18 15:00:45 +0300

    change UI/UX, and add button and click listeners of CurrentWeatherWidget

--- app/src/main/java/com/bzahov/weatherapp/ui/remoteviews/widgets/CurrentWeatherWidget.kt
+++ app/src/main/java/com/bzahov/weatherapp/ui/remoteviews/widgets/CurrentWeatherWidget.kt
@@ -1,15 +1,19 @@
 package com.bzahov.weatherapp.ui.remoteviews.widgets
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import android.widget.RemoteViews
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.AppWidgetTarget
 import com.bzahov.weatherapp.ForecastApplication
 import com.bzahov.weatherapp.R
 import com.bzahov.weatherapp.data.db.ForecastDatabase
+import com.bzahov.weatherapp.data.repo.interfaces.CurrentForecastRepository
 import com.bzahov.weatherapp.internal.UIConverterFieldUtils
 import com.bzahov.weatherapp.ui.settings.SettingsFragment
 import kotlinx.coroutines.CoroutineScope
@@ -22,10 +26,10 @@ import kotlinx.coroutines.launch
  * App Widget Configuration implemented in [CurrentWeatherWidgetConfigureActivity]
  */
 private const val TAG = "CurrentWeatherWidget"
+private const val ONCLICKTAG = "myOnClickTag"
 
 class CurrentWeatherWidget(
-
-//    val currentWeatherRepository: CurrentForecastRepository
+    val currentWeatherRepository: CurrentForecastRepository
 ) : AppWidgetProvider()/*, KodeinAware*/ {
     //
 //    override val kodein by closestKodein()
@@ -36,6 +40,14 @@ class CurrentWeatherWidget(
     ) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
+
+            val remoteViews = RemoteViews(context.packageName, R.layout.weather_widget)
+
+            remoteViews.setOnClickPendingIntent(
+                R.id.widgetRefreshData,
+                getPendingSelfIntent(context, ONCLICKTAG)
+            )
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
             updateAppWidget(
                 context,
                 appWidgetManager,
@@ -57,15 +69,33 @@ class CurrentWeatherWidget(
         }
     }
 
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+        if (intent != null) {
+            if (ONCLICKTAG == intent.action){
+                Toast.makeText(context,"Trying to refresh data",Toast.LENGTH_SHORT).show()
+               // currentWeatherRepository.requestRefreshOfData()
+            }
+        }
+    }
+
     override fun onEnabled(context: Context) {
         // Enter relevant functionality for when the first widget is created
         super.onEnabled(context)
-
     }
 
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
         super.onDisabled(context)
+    }
+
+    private fun getPendingSelfIntent(
+        context: Context?,
+        action: String?
+    ): PendingIntent? {
+        val intent = Intent(context, javaClass)
+        intent.action = action
+        return PendingIntent.getBroadcast(context, 0, intent, 0)
     }
 
     companion object {
@@ -77,7 +107,7 @@ class CurrentWeatherWidget(
         ) {
             Log.d(TAG, "update current weather widget")
             val unitAbbreviation = UIConverterFieldUtils.chooseLocalizedUnitAbbreviation(
-                context.getSharedPreferences(""/*REWORK: FIND RIGHT PREFERENCE NAME*/,0)
+                context.getSharedPreferences(""/*REWORK: FIND RIGHT PREFERENCE NAME*/, 0)
                     .getBoolean(SettingsFragment.UNIT_SYSTEM, true),
                 ForecastApplication.getAppString(R.string.metric_temperature),
                 ForecastApplication.getAppString(R.string.imperial_temperature)
@@ -128,6 +158,7 @@ class CurrentWeatherWidget(
                 unitAbbreviation
             )
 
+
             var appWidgetTarget =
                 AppWidgetTarget(context, R.id.widgetWeatherIcon, remoteViews, appWidgetId)
 
@@ -139,4 +170,6 @@ class CurrentWeatherWidget(
             return remoteViews
         }
     }
+
+
 }
